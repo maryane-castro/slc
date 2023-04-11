eafa26aa6518b2b0603fc77bc445a5c5202583b5
maryane-castro <maryane.castro993@gmail.com>
2023-04-11 20:43:49 -0300

    .

--- slc/myapp/urls.py
+++ slc/myapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-
+app_name = "myapp"
 urlpatterns = [
     path('', views.index, name='index'),
     path('novo', views.novalista, name='novalista')
