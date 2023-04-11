daf640a16c79204b542808c3c40dca7f70095065
maryane-castro <maryane.castro993@gmail.com>
2023-04-11 15:59:27 -0300

    .

--- slc/myapp/models.py
+++ slc/myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class lista(models.Model):
+    name_lista = models.CharField(max_length=100)
+
+
+class produtos(models.Model):
+    name_produto = models.CharField(max_length=100)
